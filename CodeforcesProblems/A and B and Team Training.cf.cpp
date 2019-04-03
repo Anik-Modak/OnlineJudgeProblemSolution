abb2ba104205bb86ec3700c17f4a185f6b9675a0
Anik-Modak <anikmodak.rucse@gmail.com>
2019-04-04 01:28:56 +0600

    Update A and B and Team Training.cf.cpp

--- CodeforcesProblems/A and B and Team Training.cf.cpp
+++ CodeforcesProblems/A and B and Team Training.cf.cpp
@@ -1,6 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
 {
     int n,m,ans;
+    cin>>n>>m;
     ans=(n+m)/3;
+    if(m<ans) cout<<m<<endl;
     else if(n<ans) cout<<n<<endl;
     else cout<<ans<<endl;
 }
